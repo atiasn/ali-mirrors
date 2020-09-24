b24e13cc0d38c682d4b7ef9cc2a6805f798f9c43
ritboylei@qq.com <ritboylei@qq.com>
2020-09-24 17:50:04 +0800

    更新 alpine-apk-mirros.sh

--- alpine-apk-mirros.sh
+++ alpine-apk-mirros.sh
@@ -4,7 +4,7 @@
 cp /etc/apk/repositories /etc/apk/repositories-bak
 
 # to aliyun repo
-sudo sed -i "s|http://dl-cdn.alpinelinux.org|https://mirrors.aliyun.com|g" /etc/apk/repositories
+sed -i "s|http://dl-cdn.alpinelinux.org|https://mirrors.aliyun.com|g" /etc/apk/repositories
 
 # apk update 
-sudo apt-get update
+apk update
